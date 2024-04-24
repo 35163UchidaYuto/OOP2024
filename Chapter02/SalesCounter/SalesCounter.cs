d4ffa25d44b9b9c7e28c2c276f3f2ddc13a38dfe
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-04-24 14:08:46 +0900

    メソッド移動

--- Chapter02/SalesCounter/SalesCounter.cs
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
@@ -10,12 +11,28 @@ namespace SalesCounter {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePass) {
+            _sales = ReadSales(filePass);
         }
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+       private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
         //店舗別の売り上げを求める
-        public Dictionary<string,int> GetStoreSales() {
-            Dictionary<string,int>dict = new Dictionary<string,int>();
+        public Dictionary<string, int> GetStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
