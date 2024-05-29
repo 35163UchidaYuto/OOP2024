baf61a28ae0026e7aa32505c3f14d4ffa80d71ba
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-05-29 11:45:06 +0900

    4.1完成(自分で)

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+        //4.1.1
+        public int Year {  get; private set; }
+        public int Month {  get; private set; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+ 
+
+        //4.1.2.
+        public bool Is21Century {
+            get {
+                return (Year >= 2001 && Year <= 2100);
+            }
+        }
+        //4.1/3
+        public YearMonth AddOneMonth() {
+            YearMonth yearmonth = new YearMonth(Year,Month);
+            if(Month == 12) {
+                Month = 1;
+                Year++;
+            } else {
+                Month++;
+            }
+            return yearmonth;
+
+            
+        }
+
+        //4.1.4
+        public override string ToString() {
+            return Year+"年"+Month+"月";
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,35 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+            new YearMonth(1980, 1),
+            new YearMonth(1990, 4),
+            new YearMonth(2000, 7),
+            new YearMonth(2010, 9),
+            new YearMonth(2020, 12),
+};
+
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+    }
+}
