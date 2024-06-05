1984cc14a81a2300f7b598fbe263d1ffb06151ae
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-06-05 13:12:00 +0900

    第6章演習問題

---
+++ Chapter06/Section/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            var books = Books.GetBooks().OrderByDescending(b=>b.Price).ToList();
+
+            foreach (var book in books) {
+                Console.WriteLine(book.Title + ":"+book.Price);
+            }
+            
+        }
+    }
+}
