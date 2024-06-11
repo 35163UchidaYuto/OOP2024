49f01f6f19f72af762e4cb8e1c93f0011f990d6b
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-06-11 15:52:48 +0900

    メソッド化と強制終了

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -6,21 +6,23 @@ using System.Threading.Tasks;
 
 namespace Section01 {
     internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
         static void Main(string[] args) {
-            var prefOfficeDict = new Dictionary<string, string>();
+
+            String pref, prefCaptalLocation;
             //入力処理
             Console.WriteLine("県庁所在地の登録");
-            for (int i = 0; i < 5; i++) {
+            while(true) {
                 //都道府県の入力
                 Console.Write("都道府県:");
-                String pref = Console.ReadLine();
+                pref = Console.ReadLine();
                 if(pref == null) {
-                    
+                    break;
                 }
 
                 //県庁所在地の入力
                 Console.Write("県庁所在地:");
-                String prefCaptalLocation = Console.ReadLine();
+                prefCaptalLocation = Console.ReadLine();
 
                 //既に都道府県が登録されているか？
                 if (prefOfficeDict.ContainsKey(pref)) {
@@ -30,7 +32,7 @@ namespace Section01 {
                         continue;
                     }
                 }
-                prefOfficeDict[pref] = prefCaptalLocation;
+                prefOfficeDict[pref] = prefCaptalLocation;//登録
             }
             Console.WriteLine();//改行
 
@@ -39,12 +41,12 @@ namespace Section01 {
                 switch (menuDisp()) {
                     case "1":
                         //一覧出力処理
-                        syutu(prefOfficeDict);
+                        allDisp();
                         break;
 
                     case "2":
                         //都道府県の入力
-                        nyuu(prefOfficeDict);
+                        seachPrefCaptalLocation();
                         break;
 
                     case "9":
@@ -67,13 +69,13 @@ namespace Section01 {
             return menuSelect;
         }
         //一覧表示
-        private static void syutu(Dictionary<string, string> prefOfficeDict) {
+        private static void allDisp() {
             foreach (var item in prefOfficeDict) {
                 Console.WriteLine("{0}の県庁所在地は{1}です。", item.Key, item.Value);
             }
         }
         //検索
-        private static void nyuu(Dictionary<string, string> prefOfficeDict) {
+        private static void seachPrefCaptalLocation() {
             Console.Write("都道府県:");
             String searchPref = Console.ReadLine();
             Console.WriteLine(searchPref + "の県庁所在地は" + prefOfficeDict[searchPref] + "です");
