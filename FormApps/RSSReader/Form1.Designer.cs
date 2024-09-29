0f732e4cf855f9e9a835c92555be2bcc86b9ab74
雄翔 内田 <oibc35163@ojs.ac.jp>
2024-09-29 22:46:31 +0900

    RssReder　完成

--- FormApps/RSSReader/Form1.Designer.cs
+++ FormApps/RSSReader/Form1.Designer.cs
@@ -25,10 +25,12 @@
         private void InitializeComponent() {
             this.btGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.cbFavorite = new System.Windows.Forms.ComboBox();
             this.tbName = new System.Windows.Forms.TextBox();
             this.btRegistration = new System.Windows.Forms.Button();
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,7 +38,7 @@
             // 
             this.btGet.Location = new System.Drawing.Point(643, 25);
             this.btGet.Name = "btGet";
-            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.Size = new System.Drawing.Size(100, 23);
             this.btGet.TabIndex = 1;
             this.btGet.Text = "取得";
             this.btGet.UseVisualStyleBackColor = true;
@@ -44,6 +46,7 @@
             // 
             // lbRssTitle
             // 
+            this.lbRssTitle.BackColor = System.Drawing.Color.MistyRose;
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
             this.lbRssTitle.Location = new System.Drawing.Point(12, 110);
@@ -51,18 +54,6 @@
             this.lbRssTitle.Size = new System.Drawing.Size(217, 496);
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
-            // 
-            // webView2
-            // 
-            this.webView2.AllowExternalDrop = true;
-            this.webView2.CreationProperties = null;
-            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView2.Location = new System.Drawing.Point(250, 116);
-            this.webView2.Name = "webView2";
-            this.webView2.Size = new System.Drawing.Size(671, 490);
-            this.webView2.Source = new System.Uri("https://www.yahoo.co.jp/", System.UriKind.Absolute);
-            this.webView2.TabIndex = 4;
-            this.webView2.ZoomFactor = 1D;
             // 
             // cbFavorite
             // 
@@ -76,28 +67,60 @@
             // 
             this.tbName.Location = new System.Drawing.Point(33, 66);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(449, 19);
+            this.tbName.Size = new System.Drawing.Size(604, 19);
             this.tbName.TabIndex = 6;
             // 
             // btRegistration
             // 
-            this.btRegistration.Location = new System.Drawing.Point(521, 64);
+            this.btRegistration.Location = new System.Drawing.Point(643, 66);
             this.btRegistration.Name = "btRegistration";
-            this.btRegistration.Size = new System.Drawing.Size(75, 23);
+            this.btRegistration.Size = new System.Drawing.Size(100, 21);
             this.btRegistration.TabIndex = 7;
             this.btRegistration.Text = "登録";
             this.btRegistration.UseVisualStyleBackColor = true;
             this.btRegistration.Click += new System.EventHandler(this.btRegistration_Click);
             // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Location = new System.Drawing.Point(253, 110);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(648, 485);
+            this.webView2.TabIndex = 8;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "URLを入力";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "名前を入力";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(926, 620);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webView2);
             this.Controls.Add(this.btRegistration);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.cbFavorite);
-            this.Controls.Add(this.webView2);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btGet);
             this.Name = "Form1";
@@ -111,10 +134,12 @@
         #endregion
         private System.Windows.Forms.Button btGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
         private System.Windows.Forms.ComboBox cbFavorite;
         private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Button btRegistration;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FormApps/RSSReader/Form1.cs
+++ FormApps/RSSReader/Form1.cs
@@ -18,62 +18,108 @@ namespace RSSReader {
 
 
     public partial class Form1 : Form {
+        Dictionary<string, string> source = new Dictionary<string, string> {
+            {"主要","https://news.yahoo.co.jp/rss/topics/top-picks.xml" },
+            {"国内","https://news.yahoo.co.jp/rss/topics/domestic.xml" },
+            {"国際","https://news.yahoo.co.jp/rss/topics/world.xml" },
+            {"経済","https://news.yahoo.co.jp/rss/topics/business.xml" },
+            {"エンタメ","https://news.yahoo.co.jp/rss/topics/entertainment.xml" },
+            {"スポーツ","https://news.yahoo.co.jp/rss/topics/sports.xml" },
+            {"IT","https://news.yahoo.co.jp/rss/topics/it.xml" },
+            {"科学","https://news.yahoo.co.jp/rss/topics/science.xml" },
+            {"地域","https://news.yahoo.co.jp/rss/topics/local.xml" }
+        };
+
         List<ItemData> items;
+        private EventHandler<CoreWebView2InitializationCompletedEventArgs> webView2_CoreWebView2InitializationCompleted;
         public Form1() {
             InitializeComponent();
+            webView2.CoreWebView2InitializationCompleted += webView2_CoreWebView2InitializationCompleted;
+
+            webView2.EnsureCoreWebView2Async(null);
+            source.Add(tbName.Text, cbFavorite.Text);
+            cbFavorite.DataSource = new BindingSource(source, null);
+            cbFavorite.DisplayMember = "Key";
+            cbFavorite.ValueMember = "Value";
+            this.Resize += new System.EventHandler(this.Form_Resize);
+
+            cbFavorite.SelectedIndex = -1;
+
         }
 
+        private void Form_Resize(object sender, EventArgs e) {
+            webView2.Size = this. ClientSize - new System.Drawing.Size(webView2.Location);
+        }
+
+        //取得ボタン
         private void btGet_Click(object sender, EventArgs e) {
-            using (var wc = new WebClient()) {
-                var url = wc.OpenRead(cbFavorite.Text);
-                var xdoc = XDocument.Load(url);
+            lbRssTitle.Items.Clear();
+            var cbBoxinput = cbFavorite.Text;
 
-                items = xdoc.Root.Descendants("item").
-                   Select(item => new ItemData {
-                       Title = item.Element("title").Value,
-                       Link = item.Element("link").Value
-                   }).ToList();
+            if (cbFavorite.Text.Contains("news.yahoo.co.jp/rss/")) {
+                using (var wc = new WebClient()) {
+                    var rssurl = wc.OpenRead(cbFavorite.Text);
+                    var xdoc = XDocument.Load(rssurl);
 
+                    items = xdoc.Root.Descendants("item")
+                            .Select(item => new ItemData {
+                                Title = item.Element("title").Value,
+                                Link = item.Element("link").Value
+                            }).ToList();
 
-                foreach (var item in items) {
-                    lbRssTitle.Items.Add(item.Link);
-                    lbRssTitle.Items.Add(item.Title);
+                    foreach (var item in items) {
+                        lbRssTitle.Items.Add(item.Title);
+                    }
                 }
+            } else if (source.ContainsKey(cbBoxinput)) {
+                var selectedCategory = (KeyValuePair<string, string>)cbFavorite.SelectedItem;
+                var url = selectedCategory.Value;
+
+                using (var wc = new WebClient()) {
+                    var rssurl = wc.OpenRead(url);
+                    var xdoc = XDocument.Load(rssurl);
+
+                    items = xdoc.Root.Descendants("item")
+                            .Select(item => new ItemData {
+                                Title = item.Element("title").Value,
+                                Link = item.Element("link").Value
+                            }).ToList();
+
+                    foreach (var item in items) {
+                        lbRssTitle.Items.Add(item.Title);
+                    }
+                }
+            } else {
+                MessageBox.Show("正しいＵＲＬを入力してください", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
         }
 
+        //ウェブビュアーに表示
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
             if (webView2 != null && webView2.CoreWebView2 != null) {
-                webView2.CoreWebView2.Navigate(lbRssTitle.Text);
+                webView2.CoreWebView2.Navigate(items[lbRssTitle.SelectedIndex].Link);
             }
         }
+        //登録ボタン
+        private void btRegistration_Click(object sender, EventArgs e) {
+            if (tbName.Text != null && cbFavorite.Text.Contains("news.yahoo.co.jp/rss/")) {
+                source.Add(tbName.Text, cbFavorite.Text);
+                cbFavorite.DataSource = new BindingSource(source, null);
+                cbFavorite.DisplayMember = "Key";
+                cbFavorite.ValueMember = "Value";
+            } else {
+                MessageBox.Show("名前とＵＲＬを入力してください", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
-
-
-        private void AddName(string tbName) {
-            if (!cbFavorite.Items.Contains(tbName))
-                cbFavorite.Items.Add(tbName);
+            tbName.Clear();
+            cbFavorite.SelectedIndex = -1;
         }
-
 
         //データ格納用のクラス
         public class ItemData {
             public string Title { get; set; }
             public string Link { get; set; }
-        }
-        //コンボボックスへ登録
-        private void setCbFavorite(string favorite) {
-            if (!cbFavorite.Items.Contains(favorite))
-                cbFavorite.Items.Add(favorite);
-        }
-
-        private void btRegistration_Click(object sender, EventArgs e) {
-            setCbFavorite(cbFavorite.Text);
-            inputItemsClear();
-        }
-        private void inputItemsClear() {
-            cbFavorite.Text = "";
-            tbName.Text = "";
         }
     }
 }
