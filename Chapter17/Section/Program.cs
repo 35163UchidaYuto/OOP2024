d8e7f7f14699a3b854fcccde159304e4c2b6abc2
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-10-09 14:57:33 +0900

    templateMethodパターンおわり

---
+++ Chapter17/Section/Program.cs
@@ -0,0 +1,25 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var greetings = new List<GreetingBase> {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+
+            foreach (var greeting in greetings) {
+                string msg = greeting.GetMessage();
+                Console.WriteLine(msg);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+     class LineCounterProcessor:TextProcessor {
+        private int _count;
+        string _text;
+
+        protected override void Initialize(string line) {
+            _count = 0;    
+
+        }
+
+        protected override void Execute(string fname) {
+            _count++;
+        }
+
+        protected override void Terminate() {
+            Console.WriteLine("{0}行",_count);
+        }
+    }
+}
