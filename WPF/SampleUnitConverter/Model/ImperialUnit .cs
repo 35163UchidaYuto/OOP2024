5cf038f52ae2cb6906930e05c54d4f651e89c1fa
UchidaYuto <166583791+35163UchidaYuto@users.noreply.github.com>
2024-11-06 14:22:12 +0900

    SampleUnitConverter完成

---
+++ WPF/SampleUnitConverter/Model/ImperialUnit .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    public class ImperialUnit :DistanceUnit{
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit{Name = "in",coefficient = 1,},
+            new ImperialUnit{Name = "ft",coefficient = 12,},
+            new ImperialUnit{Name = "yd",coefficient = 12 * 3,},
+            new ImperialUnit{Name = "ml",coefficient = 12 * 3 * 1760,},
+        };
+        public static ICollection<ImperialUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// メートル単位からヤード単位に変換します
+        /// </summary>
+        /// <param name="unit">メートル単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromMetricUnit(MetricUnit unit, double value) { 
+            return (value * unit.coefficient)/ 25.4/ this.coefficient;
+        }
+    }
+}
---
+++ WPF/SampleUnitConverter/Model/MetricUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    public class MetricUnit :DistanceUnit{
+        private static List<MetricUnit> units = new List<MetricUnit> {
+            new MetricUnit{Name = "mm",coefficient = 1,},
+            new MetricUnit{Name = "cm",coefficient = 10,},
+            new MetricUnit{Name = "m",coefficient = 10*100,},
+            new MetricUnit{Name = "km",coefficient = 10*100*1000,},
+        };
+        public static ICollection<MetricUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します
+        /// </summary>
+        /// <param name="unit">ヤード単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromImperialUnit(ImperialUnit unit, double value) { 
+            return (value * unit.coefficient)* 25.4/ this.coefficient;
+        }
+    }
+}
